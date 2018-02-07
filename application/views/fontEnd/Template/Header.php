9a5447281d7cefc4d8d2d22fc5a44a61bfe80c21
flookkiiz <passakorn8110@gmail.com>
2018-02-07 11:44:29 +0700

    herrd

--- application/views/fontEnd/Template/Header.php
+++ application/views/fontEnd/Template/Header.php
@@ -14,32 +14,43 @@
   <link href="<?php echo base_url('asset/css/style.css') ?>" rel="stylesheet" />
   <link href="<?php echo base_url('asset/css/datepicker.css') ?>" rel="stylesheet" />
 
-    <header>
-		<div class="container">
-			<div class="row">
-				<nav class="navbar navbar-default" role="navigation">
-					<div class="container-fluid">
-						<div class="navbar-header">
-							<div class="navbar-brand">
-								<a href="<?php echo site_url('');?>"><h1>ม่านและมู่ลี่</h1></a>
-							</div>
-						</div>
-						<div class="menu">
-							<ul class="nav nav-tabs" role="tablist">
-								<li role="presentation" class="active"><a href="<?php echo site_url('');?>">Home</a></li>
-                <!-- <li role="presentation"><a href="<?php echo site_url('fontEnd/ShoppingCart');?>">ตะกร้าสินค้า</li></a> -->
-                <li> <a href="<?php echo base_url();?>index.php/fontEnd/Checkout"> <i class="glyphicon glyphicon-shopping-cart"></i> Checkout (<span id="update_cart"><?php if($cart_session){echo array_sum($cart_session);} else { echo '0'; } ?></span>)</a> </li>
-                <li>  <?php echo ('<a>'.'You Are : '.$this->session->userdata('username').'</a>'); ?></li>
+  <header>
+  <div class="container">
+    <div class="row">
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <div class="navbar-brand">
+              <a href="<?php echo site_url('');?>"><h1>ม่านและมู่ลี่</h1></a>
+            </div>
+          </div>
+          <div class="menu">
+            <ul class="nav nav-tabs" role="tablist">
+              <li role="presentation" class="active"><a href="<?php echo base_url();?>index.php/Home"> หน้าแรก</a></li>
+              <li > <a href="<?php echo base_url();?>index.php/fontEnd/Checkout"> <i class="glyphicon glyphicon-shopping-cart"></i> ตะกร้าสินค้า <span class="badge badge-info" id="update_cart"><?php if($cart_session){echo array_sum($cart_session);} else { echo '0'; } ?></span></a> </li>
+              <li>  <?php echo ('<a>'.''.$this->session->userdata('username').'</a>'); ?></li>
 
-								<li role="presentation"><a href="blog.html">Blog</a></li>
-								<li role="presentation"><a href="portfolio.html">Portfolio</a></li>
-								<li role="presentation"><a href="<?php echo site_url('fontEnd/login');?>">เข้าสู่ระบบ</li></a>
-                <li role="presentation"><a href= "<?php echo base_url();?>index.php/fontEnd/login/logout">Logout</a></label>
+              <li  role="presentation" >
+                <?php if($this->session->userdata('username') ==	FALSE){ ?>
+                <li role="presentation">
+                  <a href="<?php echo site_url('fontEnd/login');?>">สมัครสมาชิก/ล็อกอิน</li></a>
+                <?php }else{ ?>
+              <li role="presentation"><a href="blog.html">ข้อมูลการสั่งซื้อ</a></li>
+              <li>  <?php echo ('<a>'.''.$this->session->userdata('username').'</a>'); ?></li>
+              <li><a href= "<?php echo base_url();?>index.php/fontEnd/login/logout">ล็อกเอาท์</a></li>
+              <?php } ?></i></a>
+              </li>
 
-							</ul>
-						</div>
-					</div>
-				</nav>
-			</div>
-		</div>
-	</header>
+
+
+              <li role="presentation"><a href="portfolio.html">Portfolio</a></li>
+              <li role="presentation"><a href="<?php echo site_url('fontEnd/login');?>">สมัครสมาชิก/ล็อกอิน</li></a>
+              <li role="presentation"><a href= "<?php echo base_url();?>index.php/fontEnd/login/logout">ล็อกเอาท์</a></label>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</header>
