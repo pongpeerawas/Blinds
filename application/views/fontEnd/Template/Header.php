d300e70de41699ac7e122137cb9e51e2453c288d
flookkiiz <passakorn8110@gmail.com>
2018-02-07 11:41:34 +0700

    hedderr

--- application/views/fontEnd/Template/Header.php
+++ application/views/fontEnd/Template/Header.php
@@ -1,28 +1,19 @@
-<!DOCTYPE HTML>
-<html>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>หลังร้าน</title>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ม่านและมู่ลี่</title>
 
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link href="<?php echo base_url('assetAdmin/css/bootstrap.min.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/bootstrap-responsive.min.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/uniform.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/select2.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/matrix-style.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/matrix-media.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/font-awesome/css/font-awesome.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/fullcalendar.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/jquery.gritter.css') ?>" rel="stylesheet" />
+    <!-- Bootstrap -->
+  <link href="<?php echo base_url('asset/css/bootstrap.min.css') ?>" rel="stylesheet" />
+	<link href="<?php echo base_url('asset/css/responsive-slider.css') ?>" rel="stylesheet" />
+	<link href="<?php echo base_url('asset/css/animate.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/css/font-awesome.min.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/css/style.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/css/datepicker.css') ?>" rel="stylesheet" />
 
-
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-<!-- <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> -->
-
-</head>
     <header>
 		<div class="container">
 			<div class="row">
@@ -35,26 +26,15 @@
 						</div>
 						<div class="menu">
 							<ul class="nav nav-tabs" role="tablist">
-								<li role="presentation" class="active"><a href="<?php echo base_url();?>index.php/Home"> หน้าแรก</a></li>
-                <li > <a href="<?php echo base_url();?>index.php/fontEnd/Checkout"> <i class="glyphicon glyphicon-shopping-cart"></i> ตะกร้าสินค้า <span class="badge badge-info" id="update_cart"><?php if($cart_session){echo array_sum($cart_session);} else { echo '0'; } ?></span></a> </li>
-                <li>  <?php echo ('<a>'.''.$this->session->userdata('username').'</a>'); ?></li>
+								<li role="presentation" class="active"><a href="<?php echo site_url('');?>">Home</a></li>
+                <!-- <li role="presentation"><a href="<?php echo site_url('fontEnd/ShoppingCart');?>">ตะกร้าสินค้า</li></a> -->
+                <li> <a href="<?php echo base_url();?>index.php/fontEnd/Checkout"> <i class="glyphicon glyphicon-shopping-cart"></i> Checkout (<span id="update_cart"><?php if($cart_session){echo array_sum($cart_session);} else { echo '0'; } ?></span>)</a> </li>
+                <li>  <?php echo ('<a>'.'You Are : '.$this->session->userdata('username').'</a>'); ?></li>
 
-                <li  role="presentation" >
-                  <?php if($this->session->userdata('username') ==	FALSE){ ?>
-                  <li role="presentation">
-                    <a href="<?php echo site_url('fontEnd/login');?>">สมัครสมาชิก/ล็อกอิน</li></a>
-                  <?php }else{ ?>
-                <li role="presentation"><a href="blog.html">ข้อมูลการสั่งซื้อ</a></li>
-                <li>  <?php echo ('<a>'.''.$this->session->userdata('username').'</a>'); ?></li>
-                <li><a href= "<?php echo base_url();?>index.php/fontEnd/login/logout">ล็อกเอาท์</a></li>
-                <?php } ?></i></a>
-                </li>
-
-
-
+								<li role="presentation"><a href="blog.html">Blog</a></li>
 								<li role="presentation"><a href="portfolio.html">Portfolio</a></li>
-								<li role="presentation"><a href="<?php echo site_url('fontEnd/login');?>">สมัครสมาชิก/ล็อกอิน</li></a>
-                <li role="presentation"><a href= "<?php echo base_url();?>index.php/fontEnd/login/logout">ล็อกเอาท์</a></label>
+								<li role="presentation"><a href="<?php echo site_url('fontEnd/login');?>">เข้าสู่ระบบ</li></a>
+                <li role="presentation"><a href= "<?php echo base_url();?>index.php/fontEnd/login/logout">Logout</a></label>
 
 							</ul>
 						</div>
@@ -62,8 +42,4 @@
 				</nav>
 			</div>
 		</div>
-
-
-
-
 	</header>
