0ba4d35a9a6b5b56eb57f25449cd3ff3177e5e36
pongpeerawas <peerawat6385@gmail.com>
2018-02-07 11:32:02 +0700

    header

--- application/views/fontEnd/Template/Header.php
+++ application/views/fontEnd/Template/Header.php
@@ -1,27 +1,18 @@
-<!DOCTYPE HTML>
-<html>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>หลังร้าน</title>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ม่านและมู่ลี่</title>
 
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link href="<?php echo base_url('assetAdmin/css/bootstrap.min.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/bootstrap-responsive.min.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/uniform.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/select2.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/matrix-style.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/matrix-media.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/font-awesome/css/font-awesome.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/fullcalendar.css') ?>" rel="stylesheet" />
-<link href="<?php echo base_url('assetAdmin/css/jquery.gritter.css') ?>" rel="stylesheet" />
-
-
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-<!-- <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> -->
-
+    <!-- Bootstrap -->
+  <link href="<?php echo base_url('asset/css/bootstrap.min.css') ?>" rel="stylesheet" />
+	<link href="<?php echo base_url('asset/css/responsive-slider.css') ?>" rel="stylesheet" />
+	<link href="<?php echo base_url('asset/css/animate.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/css/font-awesome.min.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/css/style.css') ?>" rel="stylesheet" />
+  <link href="<?php echo base_url('asset/css/datepicker.css') ?>" rel="stylesheet" />
 </head>
     <header>
 		<div class="container">
